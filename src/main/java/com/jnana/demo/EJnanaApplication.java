adbf741e7f48e1dc018d22349b0c84405897e4e2
Varun-Vaitla <varunvm996@gmail.com>
2025-06-21 21:48:10 +0530

    1.Project SetUp

---
+++ src/main/java/com/jnana/demo/EJnanaApplication.java
@@ -0,0 +1,13 @@
+package com.jnana.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EJnanaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EJnanaApplication.class, args);
+	}
+
+}
