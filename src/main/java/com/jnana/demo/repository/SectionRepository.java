55177d3cec8abe5707397cfcb03ad5b6b13e66cf
Varun-Vaitla <varunvm996@gmail.com>
2025-06-25 00:54:00 +0530

    7. Add Section Part - 1

---
+++ src/main/java/com/jnana/demo/repository/SectionRepository.java
@@ -0,0 +1,12 @@
+package com.jnana.demo.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.jnana.demo.model.Course;
+import com.jnana.demo.model.Section;
+
+public interface SectionRepository extends JpaRepository<Section, Long> {
+	List<Section> findByCourse(Course course);
+}
