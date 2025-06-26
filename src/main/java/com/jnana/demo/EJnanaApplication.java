bbc057ed7941eea1212b298c60bf3cf52bae2d33
Varun-Vaitla <varunvm996@gmail.com>
2025-06-26 23:02:43 +0530

    8. Add video and Notes to Cloudinary

--- src/main/java/com/jnana/demo/EJnanaApplication.java
+++ src/main/java/com/jnana/demo/EJnanaApplication.java
@@ -2,19 +2,11 @@ package com.jnana.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
-
 @SpringBootApplication
 public class EJnanaApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(EJnanaApplication.class, args);
 	}
-	
-	@Bean
-	PasswordEncoder encoder() {
-		return new BCryptPasswordEncoder();
-	}
+
 }
